4889dd0abc689e0628afaed7cf8c42b78b7dae9e
Edgar Hernández Hernández <edgarhdzhdz17@hotmail.com>
2026-03-13 18:10:45 -0600

    Remove main application files and assets, including App.js, app.json, babel.config.js, eas.json, and various image and animation files.

--- App.js
+++
@@ -1,9 +0,0 @@
-//Importacion de bibliotecas, librerias o componentes de otros archivos
-import React from "react";
-import NavigationScreens from "./NavigationScreens"; 
-
-export default function App() {
-  return (
-    <NavigationScreens/>
-  );
-}
